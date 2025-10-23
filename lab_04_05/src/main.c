d30566fbf632a81738833c7552c7162928582002
Бондарева Варвара Андреевна <bva24u807@student.bmstu.ru>
2025-10-23 14:35:54 +0300

    some small fix

--- lab_04_05/src/main.c
+++ lab_04_05/src/main.c
@@ -1,13 +1,12 @@
-#include "data.h"
+#include "../inc/data.h"
 #include "input.h"
 #include "output.h"
 
-//
 
 int main(void)
 {
     status_t exit_code = SUCCESS_CODE;
-    menu_option_t cur_menu_opt;  // выбранная опция меню
+    menu_option_t cur_menu_opt = 0;  // выбранная опция меню
 
     print_menu();
     exit_code = input_cur_menu_opt(&cur_menu_opt);
